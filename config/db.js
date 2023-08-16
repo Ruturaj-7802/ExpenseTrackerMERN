9afbef0e8ea6c89ecb57449e1ed9521f6c7e0201
Ruturaj Patil <76171596+Ruturaj-7802@users.noreply.github.com>
2023-08-16 11:53:20 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
+      //useCreateIndex: true,
       useUnifiedTopology: true,
       //these three are beng set to true as mongoDB throw us warnings
     });
