cfe53be63a407671e34f32f5f3c19f0daa7b952a
Ruturaj Patil <76171596+Ruturaj-7802@users.noreply.github.com>
2023-08-16 11:54:04 +0530

    Update transactionRoute.js

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -7,15 +7,7 @@ const {
 } = require("../controllers/transactionController");
 
 router.route("/").get(getTransactions).post(addTransaction);
-//If we make a GET request to "/"" i.e transactionRoute through server.js
-//Then we wanna call getTransactions from transactionController
 
-//But If we make a POST request to "/"" i.e transactionRoute through server.js
-//Then we wanna call addTransaction from transactionController
-
-//We have to make separate router for deleteTransaction as it id as parameter
 router.route("/:id").delete(deleteTransaction);
-//If we make a DELETE request to "/:id"" which is an additional parameter
-//Then we wanna call deleteTransaction from transactionController
 
 module.exports = router;
